b8b44b39353adfec352023b59bf93b95e2e589c6
saicharan <saicharanreddy1300@gmail.com>
2025-04-23 16:57:54 +0530

    first commit

---
+++ src/main/java/com/example/calculator/SpringbootCalculatorApplication.java
@@ -0,0 +1,12 @@
+package com.example.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCalculatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCalculatorApplication.class, args);
+    }
+}
+
